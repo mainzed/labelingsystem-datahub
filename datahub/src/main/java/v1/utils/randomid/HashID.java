1a1fee871936db4309a4dd3ee75fc1c988bd859e
Florian Thiery <web@florian-thiery.de>
2017-03-24 09:21:22 +0100

    hashid function modified

--- datahub/src/main/java/v1/utils/randomid/HashID.java
+++ datahub/src/main/java/v1/utils/randomid/HashID.java
@@ -1,16 +1,20 @@
 package v1.utils.randomid;
 
-import java.util.Random;
+import java.util.UUID;
 import org.hashids.Hashids;
 
 public class HashID {
 	
 	public static String getHASHID() {
 		// https://github.com/jiecao-fm/hashids-java
-		Random random = new Random();
-		Hashids hashids = new Hashids(random.toString(), 12);
+		Hashids hashids = new Hashids(getUUID(), 12);
 		String hash = hashids.encode(1234567L);
 		return hash;
+	}
+	
+	private static String getUUID() {
+		UUID newUUID = UUID.randomUUID();
+		return newUUID.toString();
 	}
 
 }
